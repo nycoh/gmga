c98064c309f6443749ec676e9f090f7e3d18d455
Nicolás Albarracín <nicodare@gmail.com>
2018-03-18 11:03:40 -0300

    Init

--- src/sagas/index.js
+++
@@ -1,9 +0,0 @@
-import {takeEvery} from 'redux-saga';
-import {COUNTRIES_FETCH_REQUESTED} from '../actions';
-import fetchCountries from './user';
-
-export default function* root() {
-    yield [
-        takeEvery(COUNTRIES_FETCH_REQUESTED, fetchCountries)
-    ];
-}
